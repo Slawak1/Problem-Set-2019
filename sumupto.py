c2c52b2b217e6883997cdad7d6183205c834ff76
Slawomir Sowa <47091368+Slawak1@users.noreply.github.com>
2019-02-03 22:21:25 +0000

    Update sumupto.py

--- sumupto.py
+++ sumupto.py
@@ -8,23 +8,26 @@ import sys
 a = int (input ("Enter a positive integer, Please: "))
 print (type(a))
 
-
+# if negative number will print statement. 
 if a<0:
     print ("C'mon, really? Do you know that %d is a negative number?" %(a))
     print ("Try again, You can do it.. " )
-    sys.exit(0)
+    sys.exit(0) # will stop the program after negative number provided
 else:
-    print ("Your number is %d Thank You" %(a,))
+    print ("Your number is %d Thank You" %(a))
 
 
-
+#declaration of variables
 sum = 0
 sum_total = 0
+
+# While loop. loop will be repeted untill sum lower that a, every time added to sum_total
 while sum < a:
     
     sum = sum + 1
     sum_total = sum_total+sum
 
+# print result 
 print (sum_total)
 print ("Sum of all numbers from 0 to %d is %d" %(a, sum_total))
 print ("Yeah.. this is very good program")
