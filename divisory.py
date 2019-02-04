bd3ed9d359eba2d2feaeeff3449b3e7e553b9782
Slawomir Sowa <47091368+Slawak1@users.noreply.github.com>
2019-02-04 23:24:34 +0000

    Create divisory.py

---
+++ divisory.py
@@ -0,0 +1,13 @@
+# Problem No.3
+# Write a program that prints all numbers between 1,000 and 10,000 that are divisible
+# by 6 but not 12.
+
+a = 1000 #declaration of variable
+while a<10000:  #loop will be repeated until condition is met 
+    
+    if a%6 == 0 and a%12 != 0: # we can use modulo operator % to check is number divided, and use logical operator "and"
+        print ("%a is divided by 6 but not divided by 12" %(a)) # if both conditions are "true" statemen will be printed.
+    a += 1  # increment operator
+      
+    
+
