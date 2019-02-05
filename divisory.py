de297e98d0cd21f58ea853c96f70d1de0a6c64b7
Slawomir Sowa <47091368+Slawak1@users.noreply.github.com>
2019-02-05 19:29:46 +0000

    Update divisory.py

--- divisory.py
+++ divisory.py
@@ -5,9 +5,9 @@
 a = 1000 #declaration of variable
 while a<10000:  #loop will be repeated until condition is met 
     
-    if a%6 == 0 and a%12 != 0: # we can use modulo operator % to check is number divided, and use logical operator "and"
+    if a%6 == 0 and a%12 != 0: # we can use modulo operator % to check is number divided by 6, use logical operator "and" to check if is not divided by 12
         print ("%a is divided by 6 but not divided by 12" %(a)) # if both conditions are "true" statemen will be printed.
     a += 1  # increment operator
       
     
-
+print ('Thank You for using My program')
