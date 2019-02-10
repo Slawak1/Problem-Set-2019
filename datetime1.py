9f1afdaebde293b83a8cbc25d19de5a75bddc534
Slawak1 <sowa.slawomir@gmail.com>
2019-02-10 16:51:23 +0000

    Update datetime1.py
    Changed time to AM/PM

--- datetime1.py
+++ datetime1.py
@@ -1,6 +1,7 @@
 # Problem No 8
 # Write a program that outputs today’s date and time in the format ”Monday, January
 # 10th 2019 at 1:15pm”.
+
 import datetime
 d = datetime.datetime.now()
-print (d.strftime('%A, %B %dth %Y at %X %p '))
+print (d.strftime('%A, %B %dth %Y at %I:%M %p '))
