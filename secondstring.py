203fa3417e3aefc5227f3d62cbb23ac351c8d8f5
Slawak1 <sowa.slawomir@gmail.com>
2019-02-10 22:23:18 +0000

    Update secondstring.py
    changed WHILE to FOR loop

--- secondstring.py
+++ secondstring.py
@@ -1,17 +1,19 @@
+# Problem No. 6
 # Write a program that takes a user input string and outputs every second word.
 
 text = str (input ('Please enter a sentence: ')) 
 list = text.split(' ') # split() function allow us to separate each word and store it to list
 
-l_list = len(list) # we check lenght of list 
+l_list = len(list) # check lenght of list 
 
-i = 0
-while i <= l_list:
-    try: # we were getting an error, when list length was even. To avoid that I used TRY EXCEPT statement
-        print (list[i], end = " ") # by using argument we can print in same line
+i = 0 
+
+for i in range (0,l_list, 2):
+    try: # I was getting an error, when list length was even. To avoid that I used TRY EXCEPT statement
+        print (list[i], end = " ") # by using END argument statement can be printed in same line
     except IndexError:
         pass # skip error message when list length is even
     
-    i = i + 2 # increment by 2 to skip every second word
+    
 
 print ('\n \n Thank You for using my program') 
