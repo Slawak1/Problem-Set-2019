5be716e7dcf8e5f7bf3da79f542decbceb07800d
Slawak1 <sowa.slawomir@gmail.com>
2019-02-11 22:32:22 +0000

    Error Handling and comment added

--- second.py
+++ second.py
@@ -2,19 +2,29 @@
 # Write a program that reads in a text file and outputs every second line. The program
 # should take the filename from an argument on the command line.
 
-import sys
-text = open (sys.argv[1])
+# argv() is part of sys libreary.
+import sys 
 
-# print (text.read())
+# validate file name, if empty or wrong file name error message will be printed. 
+try:
+    text = open (sys.argv[1]) # argv() is a list of arguments separated by space. 
+except IndexError:
+    print ("No file found. As an argument please use file name, example: python second.py yourfilename.txt")
+    sys.exit()
+except FileNotFoundError:
+    print ("File not found. Check the name and try again.")    
+    sys.exit()
 
+#from input file lines are saved to list and list length is calculated.      
 list = text.readlines()
 l_list = len(list)
-print (l_list)
 
+# to print every second line i can use FOR loop with step = 2.
 i = 0
-for i in range(0, l_list+1, 2):
+for i in range(0, l_list + 1, 2):
     try:
         print (list[i], end="\n")
     except IndexError:
         pass # skip error message when list length is even
+        
 print ("\n \n Thank You for using my program")
