52f2b0fd280698c7a90baab9026736007ed404aa
Slawak1 <sowa.slawomir@gmail.com>
2019-02-11 22:00:37 +0000

    Working

--- second.py
+++ second.py
@@ -2,3 +2,19 @@
 # Write a program that reads in a text file and outputs every second line. The program
 # should take the filename from an argument on the command line.
 
+import sys
+text = open (sys.argv[1])
+
+# print (text.read())
+
+list = text.readlines()
+l_list = len(list)
+print (l_list)
+
+i = 0
+for i in range(0, l_list+1, 2):
+    try:
+        print (list[i], end="\n")
+    except IndexError:
+        pass # skip error message when list length is even
+print ("\n \n Thank You for using my program")
