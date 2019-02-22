595e4b415f059fa099c3d9aa9d91d8d79227d6d3
Slawak1 <sowa.slawomir@gmail.com>
2019-02-22 23:57:01 +0000

    comments added

--- begins-with-t.py
+++ begins-with-t.py
@@ -1,17 +1,19 @@
+# Slawomir Sowa 
+# Date: 03.02.2019
 # Problem No. 2
 # Write a program that outputs whether or not today is a day that begins with the
 # letter T. An example of running this program on a Thursday is as follows.
 
 
-import datetime
+import datetime # import datetime module which contain datetime.now() function
 day = datetime.datetime.now() # import current date and time to variable 'day'
-a = day.strftime("%A") # string format with directive %A will print full weekday name, we save it to list 'a'.
-print ("Today is %s " % (a))
+a_str = day.strftime("%A") # string format with directive %A prints full weekday name, we save it as a string 'a_str'.
+print ("Today is %s " % (a_str))
 
-#statement IF will check is first item on list 'a' same as letter T 
-if a[0] == str('T'): 
-    print ("Yes - %s begins with a letter \'T\'" % (a))
+
+if a_str[0] == str('T'): # if first item in string (with index 0) is the same as a letter T IF block is executed 
+    print ("Yes - %s begins with a letter \'T\'" % (a_str)) # if statement is true, this block is executed 
 else:
-    print ("No - %s does not begin with a \'T\'" % (a))
+    print ("No - %s does not begin with a \'T\'" % (a_str)) # if statement is false, this block is executed 
 
-print ('\n \n Thank You for using My program')
+print ('\nThank You for using My program')
