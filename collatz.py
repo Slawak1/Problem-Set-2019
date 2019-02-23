e5201c8ed1d93e9a93a36029fb3d8a90bbe2c5c8
Slawak1 <sowa.slawomir@gmail.com>
2019-02-23 21:15:20 +0000

    comments added

--- collatz.py
+++ collatz.py
@@ -6,30 +6,31 @@
 
 import sys # import system module
 
+# Validation of user data inserted
 # Try Except statement allow me to replace 'bad looking' error to defined text
 try:
     number = int (input ("Enter a positive integer, Please: "))
 except:
     print ('Provided is not a positive Integer. Start program and try again')
-    sys.exit(0) # stop program after error
+    sys.exit(0) # stops program after error
 
  
 if number < 0: # check if number is negative
-    # if negative number provided, statement will be printent.
+    # if negative number provided, statement is printend.
     print ("Sorry, but %d is a negative number." %(number))
     print ("Start program, and try again." )
     sys.exit(0) # will stop the program after negative number provided
 else:
     print ("Your number is %d Thank You" %(number))
 
-# while loop will be repeated untill number = 1 
+# while loop is repeated until number = 1 
 while number != 1:
     if number%2 == 0: # IF statement checks is division remainder of number divided by 2 is equal 0 
-        number = int (number/2) # if true number is divided by 2 and printed 
-        print (number, end=" ")
+        number = int (number/2) # if TRUE number is divided by 2 and printed 
+        print (number, end=" ") # argument end=" " allow me to print in the same line
     else:
-        number = int ((number * 3) + 1) #if not true - number is multiplied by 3 and added 1 and printed 
+        number = int ((number * 3) + 1) #if not TRUE - number is multiplied by 3 and added 1 and printed 
         print (number, end=" ")
 
-else: # if number equal 1 while loop will end and statement will be printed
+else: # if number equal 1 while loop ends, and statement is printed
     print ('\n \n Thank You for calculations') 
