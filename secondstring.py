eac023bb4d12150cf4382fa081b2652b527239e1
Slawak1 <sowa.slawomir@gmail.com>
2019-02-24 12:14:11 +0000

    comments added

--- secondstring.py
+++ secondstring.py
@@ -1,19 +1,19 @@
+# Slawomir Sowa 
+# Date: 9/02/2019
 # Problem No. 6
 # Write a program that takes a user input string and outputs every second word.
 
-text = str (input ('Please enter a sentence: ')) 
-list = text.split(' ') # split() function allow us to separate each word and store it to list
+text = str (input ('Please enter a sentence: ')) # takes user input and save it to var text as a string.
+a_list = text.split(' ') # split() function allow me to separate each word and store it to list
 
-l_list = len(list) # check lenght of list 
+l_list = len(a_list) # checks length of list 
 
-i = 0 
+#i = 0 # define i as 0 to start iterate from index 0 on list
 
 for i in range (0,l_list, 2):
     try: # I was getting an error, when list length was even. To avoid that I used TRY EXCEPT statement
-        print (list[i], end = " ") # by using END argument statement can be printed in same line
+        print (a_list[i], end = " ") # by using END argument I can print in the same line
     except IndexError:
         pass # skip error message when list length is even
     
-    
-
-print ('\n \n Thank You for using my program') 
+print ('\n\nThank You for using my program') 
