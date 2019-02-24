008aaa3ab28c2aae84fe91651da95f61fb550ac9
Slawak1 <sowa.slawomir@gmail.com>
2019-02-24 12:52:37 +0000

    comments added

--- squareroot.py
+++ squareroot.py
@@ -2,8 +2,8 @@
 # Write a program that that takes a positive floating point number as input and outputs
 # an approximation of its square root.
 
-# import moudules 'numpy', and 'cmath', from I can get functions sqrt(), around() 
-# cmath will allow me to work in complex numbers in case of negative number provided
+# import moudules 'numpy', and 'cmath', from 'numpy' I can get functions sqrt(), around() 
+# 'cmath' will allow me to work with complex numbers in case of negative number provided
 import numpy as np
 import cmath as cm
 import sys
@@ -12,23 +12,23 @@ import sys
 try:
     number = float (input ('Please enter number: ')) 
 except ValueError:
-    print ('Incorrect data, provided is not a number, start program and try again.')
-    sys.exit()
+    print ('Incorrect data, provided is not a number.')
+    sys.exit("Start program and try again.")
 
 if number < 0: # if negative number input I need to transform negative real number to complex number (a+bj),  
     #c_number - complex number 
-    c_number = cm.sqrt(complex(number,0))
+    c_number = cm.sqrt(complex(number.real,number.imag)) # complex([real],[img]), calculate square root
     print(c_number)
     #r_number - rounded number
-    r_number = np.around(c_number, decimals=1 ) #arg decimals=1 round to the first decimal place
+    r_number = np.around(c_number, decimals=1 ) #arg decimals=1 - round to the first decimal place
     print ('Approximitation of squared number is ', r_number, ' This is a complex number')
 
 else:
-    number = np.sqrt(number)
+    number = np.sqrt(number) #calculate square root 
     print ('\n Square root of provided number is', number)
 
     #r_number - rounded number
-    r_number = np.around(number, decimals=1 )
+    r_number = np.around(number, decimals=1 ) #round number to first decimal place 
     print ('Approximitation of squared number is', r_number)
 
 
