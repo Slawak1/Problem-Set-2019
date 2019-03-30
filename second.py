b7145722b12a74f0434a4f52c680ceacce0819e4
Slawak1 <sowa.slawomir@gmail.com>
2019-03-30 01:08:31 +0000

    comments added

--- second.py
+++ second.py
@@ -1,30 +1,34 @@
-#  Problem No. 9
+# Problem No. 9
+# Slawomir Sowa 
+# Date: 11.02.2019
 # Write a program that reads in a text file and outputs every second line. The program
 # should take the filename from an argument on the command line.
 
-# argv() is part of sys libreary.
+# argv() is part of sys library.
 import sys 
 
-# validate file name, if empty or wrong file name error message will be printed. 
+# validate file name, if none or wrong file name provided, error message will be printed. 
 try:
-    text = open (sys.argv[1]) # argv() is a list of arguments separated by space. 
+    text = open (sys.argv[1]) # argv() is a list of arguments separated by space and taken from command line. 
 except IndexError:
-    print ("No file found. As an argument please use file name, example: python second.py yourfilename.txt")
-    sys.exit()
-except FileNotFoundError:
-    print ("File not found. Check the name and try again.")    
-    sys.exit()
+    sys.exit("No file found. As an argument please use file name, example:$ python second.py yourfilename.txt")
+except FileNotFoundError:   
+    sys.exit("File not found. Check the name and try again.")
 
-#from input file lines are saved to list and list length is calculated.      
-list = text.readlines()
-l_list = len(list)
+# print (type(text))
+    
+a_list = text.readlines() # To read lines from file I used readline() function, readline() return a list 
+l_list = len(a_list) # also list length is calculated. 
 
-# to print every second line i can use FOR loop with step = 2.
-i = 0
-for i in range(0, l_list + 1, 2):
+# print (type(a_list)) - return list
+# print (l_list) 
+
+# to print every second line i used FOR loop with step = 2.
+i = 0 
+for i in range(0, l_list, 2):
     try:
-        print (list[i], end="\n")
-    except IndexError:
-        pass # skip error message when list length is even
+        print (a_list[i], end="\n")
+    except IndexError: # skip error message when list length is even
+        pass 
         
 print ("\n \n Thank You for using my program")
