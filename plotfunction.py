52107cd8fb46c267afe006444ebfcd99df13961e
Slawak1 <sowa.slawomir@gmail.com>
2019-03-30 02:03:29 +0000

    comments added

--- plotfunction.py
+++ plotfunction.py
@@ -1,37 +1,34 @@
 # Problem No 10
+# Problem No. 9
+# Slawomir Sowa 
+# Date: 11.02.2019
 # Write a program that displays a plot of the functions x, x2 and 2x in the range [0, 4].
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-# list_a = [] # list x
-# list_b = [] # list x**2
-# list_c = [] # list 2**x
+x = np.arange(0.0,4.0,0.2) # NumPy arange() function returns evenly spaced numeric value with step 0.2                             
 
-# i = 0
-# for i in range(0,4,1): 
-x = np.arange(0.0,4.0,0.2)                            
-
-    # list_a.append(i)
-    # b = i**2
-    # list_b.append(b)
-    # c = 2**i
-    # list_c.append(c)
+# functions calculated    
 f1 = x
-f2 = x**2
-f3 = 2**x   
+f2 = x**2 
+f3 = 2**x 
+
+# print (type(f1))
+
+plt.plot(x,f1, 'r',x,f2, 'g', x,f3,'b' ) # plot chart, different colour of lines assigned to each function
 
 
-# print(list_a)
-# print(list_b)
-# print(list_c)
+# Setup chart
+plt.title ('Chart x, $x^2$, $2^x$ in range (0,4)') # Title of chart 
+plt.xlabel('X axis') # X axis and Y axis description
+plt.ylabel('Y axis')
+plt.legend(['f1 = x', 'f2 = $x^2$', 'f3 = $2^x$'], loc='upper left') # Legend of Chart
+plt.grid (True) # shows grid on chart
 
-plt.plot(x,f1, 'r',x,f2, 'g', x,f3,'b' )
-plt.title (r'Chart x, $x^2$, $2^x$ in range (0,4)')
 
-plt.grid (True)
-plt.show()
+plt.show() # shows chart window 
 
 
 
